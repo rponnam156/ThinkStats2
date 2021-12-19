80b38349f325dd9e783834d62afad47bd5557e78
rajponnam <rajendraprasadponnam@discover.com>
2021-12-19 13:39:29 -0600

    Week3 assignments.

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -5,13 +5,62 @@ Copyright 2014 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
-from __future__ import print_function
+from __future__ import print_function, division
 
-import numpy as np
 import sys
 
 import nsfg
 import thinkstats2
+
+
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+    """Reads the NSFG respondent data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    print(df)
+    CleanFemResp(df)
+    return df
+
+
+def CleanFemResp(df):
+    """Recodes variables from the respondent frame.
+
+    df: DataFrame
+    """
+    pass
+
+
+def ValidatePregnum(resp):
+    """Validate pregnum in the respondent file.
+
+    resp: respondent DataFrame
+    """
+    # read the pregnancy frame
+    preg = nsfg.ReadFemPreg()
+
+    # make the map from caseid to list of pregnancy indices
+    preg_map = nsfg.MakePregMap(preg)
+
+    # iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.items():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+
+        # check that pregnum from the respondent file equals
+        # the number of records in the pregnancy file
+        if len(indices) != pregnum:
+            print(caseid, len(indices), pregnum)
+            return False
+
+    return True
 
 
 def main(script):
@@ -19,6 +68,12 @@ def main(script):
 
     script: string script name
     """
+    resp = ReadFemResp()
+
+    assert (len(resp) == 7643)
+    assert (resp.pregnum.value_counts()[1] == 1267)
+    assert (ValidatePregnum(resp))
+
     print('%s: All tests passed.' % script)
 
 
